b7100f4b154607a9acae3ca45d1966446f1e7598
muska310 <48363640+muska310@users.noreply.github.com>
2019-04-16 19:32:26 -0700

    Update finalDiscordBot.py

--- finalDiscordBot.py
+++ finalDiscordBot.py
@@ -1,8 +1,10 @@
-import discord
-from discord.ext import commands
+import json
 import time
 import asyncio
+import random
 
+from discord import Game
+from discord.ext.commands import Bot
 
 def read_token():
     with open("token.txt", "r") as f:
@@ -11,28 +13,45 @@ def read_token():
 
 TOKEN = read_token()
 
-client = discord.Client()
+BOT_PREFIX = ("?", "!")
 
+client = Bot(command_prefix = BOT_PREFIX)
 
-@client.event #event decorator/wrapper
+@client.event 
 async def on_ready():
-	print(f"We have loggin in as {client.user}")
-
-
-@client.event #event decorator/wrapper
-async def on_message(message):
-	if message.content == "!hello":
-		await message.channel.send("Watt up thoo") ## TEST MESSAGE
-	elif message.content == "!users":
-		await message.channel.send(f"""# of Members: {id.member_count}""")
-	elif message.content == "!logout":
-		await client.close()
-		
+	await client.change_presence(game=Game(name="with humans"))
+	print('Logged in as')
+	print(client.user.name)
+	print(client.user.id)
+	print('------')
 	
-@client.event #event decorator/wrapper
-async def on_member_join(member):
-	for channel in member.server.channels:
-		if str(channel) == "general":
-			await client.send_message(f"""Welcome to the virgins lair {member.mention} """)
+@client.command(name='8ball', 
+		description='Provides an answer to a yes/no question.',
+	       	pass_context = True)
+async def eight_ball(context):
+	possible_responses = [
+		'That is a resounding no',
+		'It is not looking likely',
+		'Too hard to tell',
+		'It is quite possible',
+		'Definitely',
+	]
+	await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
+
+@client.command()
+async def square(number):
+	squared_value = int(number) + int(number)
+	await client.say(str(number) + " squared is " + str(squared_value))
+		
+
+@client.event
+async def on_message(message):
+	if message.content.startswith('!hello'):
+		await client.send_message(message.channel, 'Hi! testBot ready!')
+	elif message.content.startswith('!flip'):
+		flip = random.choice(['Heads', 'Tails'])
+		await client.send_message(message.channel, flip)
+	
+
 
 client.run(TOKEN)
